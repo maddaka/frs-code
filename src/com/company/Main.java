0b9e998a1281cf3debc6383ab1c15249566fd0cf
siri <maddakasireesha@gmail.com>
2021-05-31 23:00:10 +0530

    added checkpoint5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        
+        Ticket regularTicket = new RegularTicket("1234", "Bangalore", "Mumbai", null, "DepartureDateTime", "ArrivalDateTime", null, 2000, false, "3A", "lunch");
+        Ticket touristTicket = new TouristTicket("5678", "Bangalore", "Goa", null, "DepartureDateTime", "ArrivalDateTime", null, 200, false, "3B", "Goa Resort", new String[]{" "});
 
-        /*Address address = new Address("TCP layout","Bangalore","Karnataka");
-        Contact contact = new Contact("luck","luck@gmail.com","123");
-
-        System.out.println(address.getAddressDetails());
-        System.out.println(contact.getContactDetails());
-    */
-
-
-
-
+        printTicketDetails(regularTicket);
+        printTicketDetails(touristTicket);
     }
+
+        public static void printTicketDetails(Ticket ticket){
+            System.out.println(ticket.getPnr());
+        }
+
+
+
+
+
 }
